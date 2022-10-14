14ce4818930485fb0a7e7e0581bf146c166a6d09
KanamyDamiao <kanamy.damiao@twygo.com>
2022-10-13 22:21:06 -0300

    adicionando cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Button, ButtonBase } from '@mui/material'
 import './App.css';
 import Contador from './components/Contator';
 import FetchNews from './components/FetchNews';
+import FetchProducts from './components/FetchProduct';
 
 
 function App() {
@@ -10,8 +11,9 @@ function App() {
       <header className="App-header">
         <h1>Olá Mundo</h1>
         <div style={{ padding: "2rem" }}>
-          <FetchNews />
-          <Contador />
+          {/* <FetchNews />
+          <Contador /> */}
+          <FetchProducts />
         </div>
       </header>
     </div>
---
+++ src/components/FetchProduct.jsx
@@ -0,0 +1,80 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+const FetchProducts = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const url = "https://dummyjson.com/products";
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((respData) => {
+        setData(respData.products);
+        setIsLoading(false);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.warn(error, "Deu erro mané!");
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <Grid container spacing={2}>
+      {isLoading ? (
+        <div>Carregando</div>
+      ) : (
+        data.map((products) => (
+          <Grid item xs={10} md={8} lg={4}>
+            <Card
+              key={products.id}
+              style={{
+                height: "100%",
+                display: "flex",
+                alignItems: "stretch",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="160"
+                  image={products.thumbnail}
+                  alt={products.brand}
+                />
+                <CardContent style={{flex:"1 1 auto"}}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {products.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {products.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Buy
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))
+      )}
+    </Grid>
+  );
+};
+
+export default FetchProducts;
